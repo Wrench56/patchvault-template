679b906209ff2875623f1815ec3d72f7d4adbb02
Mark Devenyi <markdevenyidev@gmail.com>
2026-05-31 09:10:56 -0400

    ci: add build-patchsets command to pv-ci

--- tools/pv-ci.sh
+++ tools/pv-ci.sh
@@ -18,8 +18,63 @@ lint_index() {
     fi
 }
 
+add_patchset() {
+    baseurl="$1"
+    ps="$2"
+    index="$3"
+
+    echo "Adding patchset to patchsets index"
+    echo "$baseurl/$ps $FLAGS" >> "$index"
+    sort -u -o "$index" "$index"
+}
+
+build_patchset() {
+    baseurl="$1"
+    psconf="$2"
+
+    # shellcheck disable=SC1090
+    . "$psconf"
+ 
+    pkgdir="$(dirname -- "$psconf")"
+    ps="$pkgdir/sets/$NAME.patch"
+
+    mkdir -p "$pkgdir/sets"
+    rm -f "$ps"
+    touch "$ps"
+
+    echo "Creating patchset $ps"
+    printf '%s\n' "$PATCHES" | while IFS= read -r patchname; do
+        if [ -z "$patchname" ]; then
+            continue
+        fi
+
+        patch="$pkgdir/patches/$patchname"
+        if [ ! -f "$patch" ]; then
+            echo "Warning: patch $patch not found!"
+            continue
+        fi
+        echo "  Bundling patch: $patch"
+        cat "$patch" >> "$ps"
+        echo "" >> "$ps"
+    done
+
+    add_patchset "$baseurl" "$ps" "$pkgdir/patchsets"
+    echo ""
+}
+
+build_patchsets() {
+    baseurl="$1"
+    for psconf in pkgs/*/*.conf; do
+        if [ ! -e "$psconf" ]; then
+            continue
+        fi
+
+        build_patchset "$baseurl" "$psconf"       
+    done
+}
+
 if [ "$#" -eq 0 ]; then
-    echo "Usage: pv-ci lint-index|all"
+    echo "Usage: pv-ci lint-index|build-patchsets <baseurl>|all <baseurl>"
     exit 0
 fi
 
@@ -27,8 +82,12 @@ case "$1" in
     lint-index)
         lint_index
         ;;
+    build-patchsets)
+        build_patchsets "$2"
+        ;;
     all)
         lint_index
+        build_patchsets "$2"
         ;;
     *)
         die "Unknown command: $1"
